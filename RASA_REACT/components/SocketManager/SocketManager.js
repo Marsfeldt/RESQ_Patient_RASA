1e13bdf19b55657daeada110d5b26b0d3f12d290
JordanLanglet <93703815+JordanLanglet@users.noreply.github.com>
2024-08-26 14:03:49 +0200

    change SocketManager.js
    Change addresses to communicate with application and server
    Added a log management

--- RASA_REACT/components/SocketManager/SocketManager.js
+++ RASA_REACT/components/SocketManager/SocketManager.js
@@ -1,23 +1,42 @@
 import io from 'socket.io-client';
 
 // Socket responsible for connecting to RASA (This server is running on port 5005)
-const rasaServerSocket = io('http://192.168.0.157:5005');
+const rasaServerSocket = io('http://10.0.2.2:5005', {
+    transports: ['websocket'],
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
+});
+
 
 // Socket responsible for connecting to the Python Server (This server is running on port 5006)
-const pythonServerSocket = io('http://192.168.0.157:5006');
+const pythonServerSocket = io('http://10.0.2.2:5006', {
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
+});
+
+// Event listeners for debugging
+rasaServerSocket.on('connect', () => {
+    console.log('Connected to RASA Server Socket:', rasaServerSocket.id);
+});
+
+rasaServerSocket.on('connect_error', (error) => {
+    console.error('Connection to RASA Server failed:', error);
+});
+
+rasaServerSocket.on('disconnect', (reason) => {
+    console.warn('Disconnected from RASA Server Socket:', reason);
+});
 
 // Function to connect server sockets
 const connectSockets = () => {
     // RASA
     if (!rasaServerSocket.connected) {
         rasaServerSocket.connect();
-        console.log('Connected to RASA Server Socket');
     }
 
     // PYTHON
     if (!pythonServerSocket.connected) {
         pythonServerSocket.connect();
-        console.log('Connected to Python Server Socket');
     }
 }
 
@@ -36,18 +55,16 @@ const disconnectSockets = () => {
     }
 }
 
-// Function for reconnecting sockets in case of unexpected disconnects
+// Function to reconnect server sockets
 const reconnectSockets = () => {
     // RASA
     if (!rasaServerSocket.connected) {
         rasaServerSocket.connect();
-        console.log('Reconnected to RASA Server Socket');
     }
 
     // PYTHON
     if (!pythonServerSocket.connected) {
         pythonServerSocket.connect();
-        console.log('Reconnected to Python Server Socket');
     }
 }
 
